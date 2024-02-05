8a4d63d462f21b4b41c5a53e404ed2ae3fef1720
Radek <radek.goralik@interia.pl>
2024-02-05 22:36:22 +0100

    Added Swagger2 and hidden API key

--- src/main/java/com/Forecast/Forecast/ForecastApplication.java
+++ src/main/java/com/Forecast/Forecast/ForecastApplication.java
@@ -3,7 +3,6 @@ package com.Forecast.Forecast;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 @SpringBootApplication
 @EnableFeignClients
 public class ForecastApplication {
@@ -12,4 +11,7 @@ public class ForecastApplication {
 		SpringApplication.run(ForecastApplication.class, args);
 
 	}
+
+
+
 }
--- src/main/java/com/Forecast/Forecast/WeatherService.java
+++ src/main/java/com/Forecast/Forecast/WeatherService.java
@@ -1,9 +1,11 @@
 package com.Forecast.Forecast;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
+@Component
 public class WeatherService {
 
     private final WeatherClient weatherClient;
@@ -11,13 +13,13 @@ public class WeatherService {
     private final String weatherClientApiKey;
 
     public WeatherService(WeatherClient weatherClient,
-                          @Value("${weather.client.api-key:}") String weatherClientApiKey) {
+                          @Value("${api.key}") String weatherClientApiKey) {
         this.weatherClient = weatherClient;
         this.weatherClientApiKey = weatherClientApiKey;
     }
 
     public WeatherData getWeatherData(String city) {
-        String include = "metric,hours,days";
+        String include = "hours,days";
         String unitGroup = "metric"; // "metric", "us", "uk"
 
         return weatherClient.getWeatherData(unitGroup, include, weatherClientApiKey, city);
