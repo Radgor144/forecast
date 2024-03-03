267bed8c099b9620fd3f41a72690256e3f0bb802
Radek <radek.goralik@interia.pl>
2024-03-03 21:42:43 +0100

    added WeatherClientErrorDecoder

--- src/main/java/com/Forecast/Forecast/weather/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/Forecast/Forecast/weather/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.Forecast.Forecast.weather.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler extends RuntimeException {
 
     @ExceptionHandler(CustomHttpException.class)
@@ -13,7 +16,6 @@ public class GlobalExceptionHandler extends RuntimeException {
         log.error("Error while connecting to weather api", ex);
         return new ErrorResponse("Error while connecting to weather client API.", ex.getMessage(), ex.getHttpStatus());
     }
-
 }
 
 
