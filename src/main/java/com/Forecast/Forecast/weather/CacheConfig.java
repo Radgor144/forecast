535a912750c3c3a812e7967bf9c26c83852d5ff8
Radek <radek.goralik@interia.pl>
2024-03-25 21:11:31 +0100

    added cache refresh every 10 minutes

---
+++ src/main/java/com/Forecast/Forecast/weather/CacheConfig.java
@@ -0,0 +1,32 @@
+package com.Forecast.Forecast.weather;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableCaching
+@EnableScheduling
+@Slf4j
+public class CacheConfig {
+
+    public static final String WEATHER_DATA = "WeatherData";
+
+    @Bean
+    public CacheManager cacheManager() {
+
+        return new ConcurrentMapCacheManager(WEATHER_DATA);
+    }
+
+    @CacheEvict(allEntries = true, value = {WEATHER_DATA})
+    @Scheduled(fixedDelay = 10 * 60 * 1000) // refresh cache every 10 minutes
+    public void reportCacheEvict() {
+        log.info("Refresh Cache");
+    }
+}
